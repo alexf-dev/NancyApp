2385cc1884a41e495d7a1471d5a26a1d55457056
afissenko <regalex82@mail.ru>
2020-03-02 17:41:30 +0600

    Add repository all files

---
+++ NancyApp/Models.cs
@@ -0,0 +1,94 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using Nancy.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace NancyApp
+{
+    public class Dinosaur
+    {
+        public string Name { get; set; }
+        public int HeightInFeet { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class DinosaurModule : NancyModule
+    {
+        private static Dinosaur dinosaur = new Dinosaur()
+        {
+            Name = "Tirex",
+            HeightInFeet = 117,
+            Status = "Hungry"
+        };
+
+        private static List<Dinosaur> dinosaurs = new List<Dinosaur>()
+        {
+            new Dinosaur() {
+                Name = "Tirex",
+                HeightInFeet = 117,
+                Status = "Hungry"
+            },
+            new Dinosaur() {
+                Name = "Kierkegaard",
+                HeightInFeet = 6,
+                Status = "Inflated"
+            }
+        };
+
+        public DinosaurModule()
+        {
+            Get("/dinosaurs", parameters =>
+            {
+                return dinosaurs;
+            });
+
+            Get("/dinosaurs/{id}", parameters =>
+            {
+                return dinosaurs[parameters.id - 1];
+            });
+
+            Post("/dinosaurs", parameters =>
+            {
+                var model = this.Bind<Dinosaur>();
+                dinosaurs.Add(model);
+                return dinosaurs.Count.ToString();
+            });
+        }
+    }
+
+    public class NancyRESTService : Topshelf.ServiceControl
+    {
+        public static NancyHost NHost = null;
+        public NancyRESTService()
+        {
+            NHost = new NancyHost(new Uri("http://localhost:1234"));
+        }
+
+        //public void Start()
+        //{
+        //    NHost.Start();            
+        //}
+
+        public bool Start(HostControl hostControl)
+        {
+            NHost.Start();
+            return true;
+        }
+
+        //public void Stop()
+        //{
+        //    NHost.Stop();
+        //}
+
+        public bool Stop(HostControl hostControl)
+        {
+            NHost.Stop();
+            return true;
+        }
+    }
+}
---
+++ NancyApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+using Topshelf;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var host = new NancyHost(new Uri("http://localhost:1234")))
+            //{
+            //    host.Start();
+
+            //    Console.WriteLine("NancyFX Stand alone test application.");
+            //    Console.WriteLine("Press enter to exit the application");
+            //    Console.ReadLine();
+            //}
+
+            HostFactory.Run(x => x.Service<NancyRESTService>());
+
+            //HostFactory.Run(x =>                               //1
+            //{
+
+            //    x.Service<NancyRESTService>(s =>                      //2
+            //    {
+            //        s.ConstructUsing(name => new NancyRESTService()); //3
+            //        s.WhenStarted(tc => tc.Start());           //4 
+            //        s.WhenStopped(tc => tc.Stop());            //5
+            //    });
+            //    x.RunAsLocalSystem();                          //6
+
+            //    x.SetDescription("Alex_F Sample Topshelf Host");      //7
+            //    x.SetDisplayName("Alex_F Stuff");                     //8
+            //    x.SetServiceName("Alex_F Stuff");                     //9
+            //});
+        }
+    }    
+}
